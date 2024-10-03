af593f17c8a75c8c0b392043544dde039970bdca
ravenpost <luisadoamaral@gmail.com>
2024-10-03 12:30:02 +0900

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,21 @@
 // sketch.js
 function setup() {
-  createCanvas(250, 250);
+  let canvas = createCanvas(250, 250);
+  canvas.parent(document.body);
   background(250);
+  canvas.touchStarted(touchStartedHandler);
+  canvas.touchMoved(touchMovedHandler);
+}
+
+function touchStartedHandler(event) {
+  event.preventDefault();
+}
+
+function touchMovedHandler(event) {
+  fill(0);
+  ellipse(touchX, touchY, 30, 30);
+  return false; 
 }
 
 function draw() {
-  fill(0);
-  ellipse(mouseX, mouseY, 30, 30);
 }
